15e2c53bed5c00f5cd474ff168b746e5f8496b21
chandrakanthvemakoti <chanduvemakoti@gmail.com>
2020-04-22 16:26:43 +0530

    first

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    
+    <div>
+   <app-header></app-header>
+  
+   <router-view></router-view> 
+ 
+            </div>
+       
+
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+    export default {
+    
+
+        components:{
+            appHeader: Header,
+         
+        }
+
+    }
+</script>
+
+<style>
+body, html {
+    margin: 0;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+
+</style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="dash">
+   <h1>MY DASHBOARD</h1>
+   <hr>
+   <div class="container">
+    <app-formtable></app-formtable>
+    <div>
+   
+    </div>
+    
+   </div>
+   </div>
+   </template>
+    <script>
+  
+      
+      import Formtable from './Formtable.vue'
+      export default{
+     data(){
+  return{
+
+    
+  }
+ },
+  
+          components:{
+            
+            appFormtable:Formtable
+          },
+   
+      }
+    </script>
+<style scoped>
+.dash{
+    text-align: center;
+}
+</style>
