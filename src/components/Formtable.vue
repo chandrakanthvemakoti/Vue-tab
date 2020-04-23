29d6745a2b8232b3894baba7f3ca84e0b0b7781f
chandrakanthvemakoti <chanduvemakoti@gmail.com>
2020-04-23 15:26:22 +0530

    form

--- src/components/Formtable.vue
+++ src/components/Formtable.vue
@@ -6,44 +6,35 @@
                     <h1>Form</h1>
                     <hr>
                    
-                    <div class="form-group" :class="{invalid: $v.name.$error}">
+                    <div class="form-group">
                         <label for="name">Name</label>
                         <input
                                 type="text"
                                 id="name"
                                 class="form-control"
                                 v-model="name"
-                                
-                                @blur="$v.name.$touch()"
                                >
                     </div>
-                    <p v-if="!$v.name.required" style="color:red">*please enter the name</p>
                       
-                      <div class="form-group" :class="{invalid: $v.email.$error}">
+                      <div class="form-group">
                         <label for="email">Email</label>
                         <input
                                 type="email"
                                 id="email"
                                 class="form-control"
                                 v-model="email"
-                                @blur="$v.email.$touch()"
                                 >
                     </div>
-                    <p v-if="!$v.email.email"  style="color:red">*please enter the valid email</p>
-                        <p v-if="!$v.email.required" style="color:red">*please enter the email</p>   
-                      <div class="form-group" :class="{invalid: $v.role.$error}">
+                          
+                      <div class="form-group">
                         <label for="role">role</label>
-                       <select 
-                        id="role"
-                        class="form-control"
-                        v-model="role"
-                         @blur="$v.role.$touch()">
-                          <option value="" disabled selected hidden>Please Select</option>
-
-                  <option v-for="ro in roles">{{ro}}</option>
-                       </select>
-                    </div>  
-                     <p v-if="!$v.role.required" style="color:red">*please select the role</p>   
+                        <input
+                                type="text"
+                                id="role"
+                                class="form-control"
+                                v-model="role"
+                                >
+                    </div>     
 
                     </div>
             </div>
@@ -51,10 +42,7 @@
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
                             class="btn btn-primary"
-                            @click="addnew"
-                            :disabled="$v.$invalid"
-
-                            >Submit
+                            @click="addnew">Submit
                     </button>
                 </div>
             </div>
@@ -64,9 +52,6 @@
 <br>
  
 
- <div>
-    <app-table :items="employee" :columns="headers" ></app-table>
-    </div>
                    
 
 
@@ -75,27 +60,25 @@
    
 </template>
 <script>
-import {required,email} from 'vuelidate/lib/validators'
-import Table from './Table.vue'
 
 export default {
    data(){
        return{
-      
-     headers: [ 'name', 'email', 'role'],
+     
+       newElement:[{
       name:'',
       email:'',
-      roles: [' js developer','vue developer','node developer'],
       role:'',
+}],
       employee:[]
-    
-       }
+      }
+
 
    },
-
-
    methods:{
        addnew: function(e){
+           e.preventDefault();
+
            e.preventDefault();
            var my_object ={
               
@@ -104,28 +87,10 @@ export default {
                role:this.role
            };
            this.employee.push(my_object)
-           
+          this.$emit('myemit',this.employee)
+         
 
-    //      this.users.push({name:this.newElement.name,email:this.newElement.email,role:this.newElement.role});
-    //   this.newElement = {name:"",email:"",role:""};
-       }
-   },
-
-   components:{
-       appTable: Table
-   },
-   validations:{
-       name:{
-           required
-       },
-       email:{
-           required,
-           email
-       },
-       role:{
-           required
        }
    }
-   
 }
 </script>
